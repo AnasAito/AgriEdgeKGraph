453de6dcfc668eb3bc69825e5cad9f1a61eeed9d
AnasAito <khdimaty@gmail.com>
2021-06-09 15:26:37 +0100

    add routing

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
-import './App.css';
-
+import "./App.css";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import Home from "./Screens/Home";
+import Explorer from "./Screens/Explorer";
 function App() {
   return (
-    <div className="App  text-center">
-    {/* Delete the two lines below */}
-    <h1 className="text-4xl font-bold pt-20">This is a starter template for you!</h1>
-    <h1 className="text-2xl font-bold pt-10">All the best with your React + Tailwind project! 😃</h1>
-    <a href="https://github.com/tanmayhinge/react-tailwind-template" target="_blank" rel="noreferrer" className="text-blue-500 underline">Read Documentation for this Template</a>
-    </div>
+    <Router>
+      {/*   <Link to="/">Home</Link>*/}
+
+      <Switch>
+        <Route path="/home">
+          <Home />
+        </Route>
+        <Route path="/explorer">
+          <Explorer />
+        </Route>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
